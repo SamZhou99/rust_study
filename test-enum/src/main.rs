444d0943400a95d0d75db2bbfc15b2b1262a9b27
sam.z <45636987@qq.com>
2025-08-18 10:52:52 +0800

    Update main.rs

--- test-enum/src/main.rs
+++ test-enum/src/main.rs
@@ -15,6 +15,21 @@ struct IpAddr {
     address: String,
 }
 
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+fn option_print(opt: Option<&str>) {
+    match opt {
+        Option::Some(something) => {
+            println!("{}", something);
+        }
+        Option::None => {
+            println!("opt is nothing");
+        }
+    }
+}
 fn main() {
     let home = IpAddr {
         kind: IpAddrKind::V4,
@@ -28,4 +43,10 @@ fn main() {
 
     println!("{:?}", home.kind);
     println!("{:?}", loopback.address);
+
+    let opt: Option<&str> = Option::Some("Hello option & match");
+    option_print(opt);
+
+    let opt2: Option<&str> = Option::None;
+    option_print(opt2);
 }
